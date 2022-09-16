f7717a33e7b3a1bd449361105c4def5921f94c11
Tien Dat <tonguyentiendat@gmail.com>
2022-09-16 21:46:09 +0700

    header ui

--- src/components/Layout/Components/Header/Header.tsx
+++ src/components/Layout/Components/Header/Header.tsx
@@ -1,9 +1,45 @@
 import './Header.scss';
+import images from '~/assets/images';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCircleXmark,
+  faSpinner,
+  faMagnifyingGlass,
+} from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
   return (
     <header className="wrapper">
-      <div className="inner"></div>
+      <div className="inner">
+        <img src={images.logo} alt="Tiktok" />
+        <div className="search">
+          <input
+            type="text"
+            placeholder="Search accounts and videos"
+            spellCheck={false}
+          />
+          <button className="search-clear">
+            {
+              //@ts-ignore
+              <FontAwesomeIcon icon={faCircleXmark} />
+            }
+          </button>
+          {/* Loading */}
+          {
+            //@ts-ignore
+            <FontAwesomeIcon className="loading" icon={faSpinner} />
+          }
+
+          <button className="search-btn">
+            {
+              //@ts-ignore
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            }
+          </button>
+        </div>
+        <div className="actions"></div>
+      </div>
     </header>
   );
 };
---
+++ src/global.d.ts
@@ -0,0 +1,12 @@
+// declare namespace JSX {
+//   interface ExtendedButton
+//     extends React.DetailedHTMLProps<
+//       React.ButtonHTMLAttributes<HTMLButtonElement>,
+//       HTMLButtonElement
+//     > {
+//     customAttribute?: string;
+//   }
+//   interface IntrinsicElements {
+//     button: ExtendedButton;
+//   }
+// }
